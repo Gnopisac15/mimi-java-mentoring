82534d490fcdc67997232071657b0526e01a7244
TibsGracia <marygracetiburillo@gmail.com>
2021-04-06 14:34:32 +0800

    Initial commit

---
+++ W1/ExercisesW1.java
@@ -0,0 +1,78 @@
+public class ExercisesW1 {
+
+    public static void main(String []args){
+
+        // Call the exercise 1 method and display the result.
+        System.out.println("Ex 1 result : "+ getCharacter("Suwayan nato",2));
+        System.out.println("Ex 2 result : "+ getCharacter("Java Exercises", 10));
+     
+        // Call the exercise 2 method, and display the result
+
+
+        // Call the exercise 3 method, and display the result
+
+
+        // Call the exercise 4 method
+        declareVariables();
+
+        // Call the exercise 5 method and display the result
+
+
+        // Call the exercise 6 method and display the result
+
+
+        // Call the exercise 7 method
+        isIntIsOdd();
+
+        // Call the exercise 8 method and display the result
+
+        // Call the exercise 9 method and display the result
+
+    }
+
+
+    // Exercise 1
+   public static char getCharacter(String word, int index) {
+        char char1 = word.charAt(index);           
+        return char1;
+//        // TODO : Complete the function
+   }
+
+    // Exercise 2
+   public static String surroundWord(String out, String word) {
+       
+       
+       
+       
+       
+       // TODO : complete the function
+    return null;
+    }
+
+    // Exercise 3
+    // TODO : write the getRectanglePerimeter function
+
+    // Exercise 4
+    public static void declareVariables(){
+//        TODO : Complete the function
+    }
+
+    // Exercise 5
+    // TODO : write the convertFloatToInteger function
+
+    // Exercise 6
+    // TODO : write the transformString function
+
+    // Exercise 7
+    public static void isIntIsOdd(){
+//        TODO : Complete the function
+    }
+
+    // Exercise 8
+    // TODO : write the sum function
+
+    // Exercise 9 BONUS
+//    public static String surroundWordBis(StringBuilder out, StringBuilder word) {
+//        // TODO
+//    }
+}
